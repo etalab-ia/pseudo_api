ca486323042ebbf2e4634f5c5fcf59c903f870fe
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-05-11 20:02:36 +0200

    added stats endpoint

--- pseudo_api/app.py
+++ pseudo_api/app.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -5,8 +6,9 @@ import stopwatch
 from flair.models import SequenceTagger
 from flask import Flask
 from flask import request, jsonify
+from sqlitedict import SqliteDict
 
-from data_ETL import prepare_output, sw
+from data_ETL import prepare_output, sw, update_stats
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -18,8 +20,14 @@ PSEUDO_MODEL_PATH = os.environ.get('PSEUDO_MODEL_PATH', './model/best-model.pt')
 TAGGER = SequenceTagger.load(PSEUDO_MODEL_PATH)
 
 
+def run_stats_request():
+    stats_dict = SqliteDict('./api_stats.sqlite', autocommit=True)
+    return jsonify(dict(stats_dict))
+
+
 def run_pseudonymize_request():
     data = {"success": False}
+    stats_dict = SqliteDict('./api_stats.sqlite', autocommit=True)
     output_types = ["pseudonymized", "tagged", "conll"]
     try:
         if not request.form.get("output_type"):
@@ -35,14 +43,19 @@ def run_pseudonymize_request():
             text = request.form.get("text")
             logging.info("Tagging text with model...")
             # Predict and return a CoNLL string to send to the web demo app
-            output = prepare_output(text=text, tagger=TAGGER, output_type=output_type)
+            output, analysis_ner_stats = prepare_output(text=text, tagger=TAGGER, output_type=output_type)
             data[output_type] = output
             data["success"] = True
-
+            # stats_dict[:]
     except Exception as e:
         logger.error(e)
     finally:
         logger.info(stopwatch.format_report(sw.get_last_aggregated_report()))
+        if data["success"]:
+            update_stats(analysis_stats=stats_dict, analysis_ner_stats=analysis_ner_stats,
+                         time_info=sw.get_last_aggregated_report(), output_type=output_type)
+        logger.info(json.dumps(dict(stats_dict), indent=4))
+        stats_dict.close()
         return jsonify(data)
 
 
@@ -52,3 +65,11 @@ def pseudonymize():
         return 'The model is up and running. Send a POST request'
     else:
         return run_pseudonymize_request()
+
+
+@server.route('/api_stats/', methods=['GET'])
+def stats():
+    if request.method == 'POST':
+        return 'The model is up and running. Send a GET request'
+    else:
+        return run_stats_request()
--- pseudo_api/data_ETL.py
+++ pseudo_api/data_ETL.py
@@ -1,27 +1,39 @@
 import copy
 import itertools
+from collections import defaultdict, Counter
 from string import ascii_uppercase
 from typing import Callable, List, Tuple
 
 from flair.data import Token, Sentence
 from sacremoses import MosesTokenizer, MosesDetokenizer, MosesPunctNormalizer
+from sqlitedict import SqliteDict
 
 moses_detokenize = MosesDetokenizer(lang="fr")
 import stopwatch
+
 sw = stopwatch.StopWatch()
 
-def create_conll_output(sentences_tagged: List[Sentence]) -> str:
+
+def read_db():
+    stats = SqliteDict('./my_db.sqlite', autocommit=True)
+    pass
+
+
+def create_conll_output(sentences_tagged: List[Sentence]):
+    tags = []
     conll_str: str = ""
     for sent_pred in sentences_tagged:
+        tags.extend([s.tag for s in sent_pred.get_spans("ner")])
         for tok_pred in sent_pred:
             result_str = f"{moses_detokenize.tokenize([tok_pred.text])}\t{tok_pred.get_tag('ner').value}\t" \
                          f"{tok_pred.start_pos}\t{tok_pred.end_pos}"
             conll_str += result_str + "\n"
         conll_str += "\n"
-    return conll_str
+    return conll_str, Counter(tags)
 
 
 def prepare_output(text: str, tagger, word_tokenizer=None, output_type: str = "pseudonymized"):
+    stats_dict = {}
     with sw.timer("root"):
         if not word_tokenizer:
             tokenizer = MOSES_TOKENIZER
@@ -37,22 +49,59 @@ def prepare_output(text: str, tagger, word_tokenizer=None, output_type: str = "p
                                               verbose=True)
 
         if output_type == "conll":
-            api_output = create_conll_output(sentences_tagged=sentences_tagged)
+            api_output, tags_stats = create_conll_output(sentences_tagged=sentences_tagged)
         elif output_type == "tagged":
-            api_output = create_tagged_text(sentences_tagged=sentences_tagged)
+            api_output, tags_stats = create_tagged_text(sentences_tagged=sentences_tagged)
         elif output_type == "pseudonymized":
-            api_output = create_pseudonymized_text(sentences_tagged=sentences_tagged)
+            api_output, tags_stats = create_pseudonymized_text(sentences_tagged=sentences_tagged)
 
-        return api_output
+        # deal with stats
+        stats_dict["nb_analyzed_sentences"] = len(text)
+        stats_dict.update(tags_stats)
+        return api_output, stats_dict
+
+
+def update_stats(analysis_stats: dict, analysis_ner_stats: dict, time_info: stopwatch.AggregatedReport,
+                 output_type: str):
+    def update_averages(avg: float, size: int, value: float):
+        return (size * avg + value) / (size + 1)
+
+    # get previous values
+    old_nb_analyzed_documents = analysis_stats.get("nb_analyzed_documents", 0)
+    old_nb_analyzed_sentences = analysis_stats.get("nb_analyzed_sentences", 0)
+    old_output_types_freq = analysis_stats.get(f"output_type_{output_type}", 0)
+    old_avg_time = analysis_stats.get("avg_time_per_doc", 0)
+    old_avg_time_per_sent = analysis_stats.get("avg_time_per_sentence", 0)
+
+    analysis_stats["nb_analyzed_documents"] = old_nb_analyzed_documents + 1
+    analysis_stats["nb_analyzed_sentences"] = old_nb_analyzed_sentences + analysis_ner_stats.pop(
+        "nb_analyzed_sentences")
+
+    # add entities tags freqs
+    analysis_stats.update(analysis_ner_stats)
+
+    # deal with time stats
+    delta_ms, _, _ = time_info.aggregated_values["root"]
+    analysis_stats["avg_time_per_doc"] = update_averages(old_avg_time,
+                                                         old_nb_analyzed_documents, delta_ms)
+    analysis_stats["avg_time_per_sentence"] = update_averages(old_avg_time_per_sent,
+                                                          old_nb_analyzed_sentences,
+                                                          delta_ms / analysis_stats[
+                                                              "nb_analyzed_sentences"])
+
+    analysis_stats[f"output_type_{output_type}"] = old_output_types_freq + 1
 
 
 def create_tagged_text(sentences_tagged: List[Sentence]):
     # Iterate over the modified sentences to recreate the text (tagged)
     tagged_str = ""
+    tags = []
     for sent in sentences_tagged:
+        tags.extend([s.tag for s in sent.get_spans("ner")])
         temp_str = sent.to_tagged_string()
         tagged_str += temp_str + "\n\n"
-    return tagged_str
+
+    return tagged_str, Counter(tags)
 
 
 def create_pseudonymized_text(sentences_tagged: List[Sentence]):
@@ -61,15 +110,18 @@ def create_pseudonymized_text(sentences_tagged: List[Sentence]):
     pseudos = singles + doubles
     pseudo_entity_dict = {}
     sentences_pseudonymized = copy.deepcopy(sentences_tagged)
+    tag_stats = defaultdict(int)
 
     # Replace the entities within the sentences themselves
     for id_sn, sent in enumerate(sentences_pseudonymized):
         for sent_span in sent.get_spans("ner"):
             if "LOC" in sent_span.tag:
                 for id_tok in range(len(sent_span.tokens)):
+                    tag_stats[sent_span.tokens[id_tok].get_tag('ner').value] += 1
                     sent_span.tokens[id_tok].text = "..."
             else:
                 for id_tok, token in enumerate(sent_span.tokens):
+                    tag_stats[token.get_tag('ner').value] += 1
                     replacement = pseudo_entity_dict.get(token.text.lower(), pseudos.pop(0))
                     pseudo_entity_dict[token.text.lower()] = replacement
                     sent_span.tokens[id_tok].text = replacement
@@ -81,7 +133,7 @@ def create_pseudonymized_text(sentences_tagged: List[Sentence]):
                                                       return_str=True)
         pseudonymized_str += detokenized_str + "\n\n"
 
-    return pseudonymized_str
+    return pseudonymized_str, tag_stats
 
 
 def create_api_output(sentences_tagged: List[Sentence]) -> Tuple[str, str]:
